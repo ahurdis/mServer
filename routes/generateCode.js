011ab1e80fbbfa8fd7368491f658574341ac08da
Andrew Hurdis <ahurdis@hotmail.com>
2018-01-12 08:46:58 -0500

    object destructuring

--- routes/generateCode.js
+++ routes/generateCode.js
@@ -9,7 +9,7 @@ router.get('/spark', function (req, res, next) {
 
   let graph = GraphURLParser.getGraph(req, res);
 
-  let codeGenerator = new CodeGenerator( { graph: graph } );
+  let codeGenerator = new CodeGenerator( graph );
 
   let strCode = codeGenerator.generateCode();
 
