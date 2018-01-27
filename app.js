89a4ddb4b4fcaa5c0f63dcf62c6bd748e2618881
Andrew Hurdis <ahurdis@hotmail.com>
2018-01-26 22:22:29 -0500

    removed unused views

--- app.js
+++ app.js
@@ -14,10 +14,6 @@ var sourceMetadata = require('./routes/sourceMetadata');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
 // Add headers
 app.use(function (req, res, next) {
 
@@ -52,11 +48,6 @@ app.use('/graphRoute', graphRoute);
 app.use('/mysql', mysql);
 app.use('/fileUpload', fileUpload);
 app.use('/sourceMetadata', sourceMetadata);
- 
-app.post('/mysqlconnection', function (req, res) {
-    console.log(req.body.host + ' and ' + req.body.password);
-    res.render('mysql', { title: 'Bitforge - Contact', msg: 'Message sent! Thank you, ' + req.body.user, err: false, page: 'contact' });
-});
 
 /*
 // catch 404 and forward to error handler
